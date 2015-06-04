b22368cd336d92dc8ce85b9ebd3a92257449b434
Dominic Tassio <dmattt98@gmail.com>
2015-06-03 23:06:29 -0500

    Initial file structure
    +basic gulp file
    +directory structure
    +empty files
    +colors
    +red

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var cssnext = require('gulp-cssnext');
+
+gulp.task('css', function() {
+	gulp.src('src/css/post-material.css')
+		.pipe(cssnext({
+			sourcemap: true
+		}))
+		.pipe(gulp.dest('./dist/css'));
+});
