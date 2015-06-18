a5d5cf80faa7c0a9feb156a75e2f24d09d052e5a
Dominic Tassio <dominc.tassio@gmail.com>
2015-06-18 15:26:08 -0500

    Component work
    +components/buttons
    +utils/globals
    +utils/normalize

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,10 +4,10 @@ var cssnext = require('gulp-cssnext');
 gulp.task('css', function() {
 	gulp.src('src/css/post-material.css')
 		.pipe(cssnext({
-			sourcemap: true,
-            compress: true
+			sourcemap: true
 		}))
-		.pipe(gulp.dest('./dist/css'));
+		.pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./docs/css'));
 });
 
 gulp.task('default', ['css']);
