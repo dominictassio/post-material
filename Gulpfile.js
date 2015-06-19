a1d04b7781eed4c49cbcb4f6b645598ac3ea4646
Dominic Tassio <dominc.tassio@gmail.com>
2015-06-19 03:46:45 -0500

    Fixes and new content
    +fix colors
    +change Gulpfile
    +change shadows
    +typography
    +cards
    +docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
-var cssnext = require('gulp-cssnext');
+var postcss = require('gulp-postcss');
+
+var cssnext = require('cssnext');
+var mixins = require('postcss-mixins');
 
 gulp.task('css', function() {
-	gulp.src('src/css/post-material.css')
-		.pipe(cssnext({
-			sourcemap: true
-		}))
-		.pipe(gulp.dest('./dist/css'))
-        .pipe(gulp.dest('./docs/css'));
+    gulp.src('./src/css/post-material.css')
+        .pipe(postcss([
+            cssnext({
+                sourcemap: true
+            }),
+            mixins()
+        ]))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./docs/css/'));
 });
 
 gulp.task('default', ['css']);
